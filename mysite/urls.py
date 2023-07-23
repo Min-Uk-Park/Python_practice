48878a9791b82e6dd24e67779a6b65c91cda30aa
ParkMinUk <parkmxxuk@gmail.com>
2023-07-23 16:29:21 +0900

    장고 이미지 띄우기 연습

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import weatherfind.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home',weatherfind.views.home,name='home')
+    # path('home',weatherfind.views.home,name='home')
+    path('home/',include('weatherfind.url')),
 ]
+
